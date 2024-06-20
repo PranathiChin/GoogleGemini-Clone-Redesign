761aeb045c011a8b7427788664f950057a47e338
Pranathi Chinthalapani <ch.pranathireddy123@gmail.com>
2024-06-20 14:38:51 -0400

    initial commit to github

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is my first functional component using the rafce snippet.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
